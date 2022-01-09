14f2ce48621c4f7b9dd203508bb7abed93690b42
Tangwh <846765179@qq.com>
2022-01-09 11:29:07 +0800

    完成PermissionX库的开发禹测试工作

--- app/src/main/java/com/permissionx/app/MainActivity.kt
+++ app/src/main/java/com/permissionx/app/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.permissionx.app
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.permissionx.library.PermissionX
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    private fun call() {
+
+        try {
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:10086")
+            startActivity(intent)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    fun callTel(view: android.view.View) {
+        PermissionX.request(
+            this,
+            android.Manifest.permission.CALL_PHONE,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            android.Manifest.permission.READ_CONTACTS
+        ) { allGranted, deniedList ->
+
+                if(allGranted){
+                    call()
+                }else{
+                    Toast.makeText(this,"You denied $deniedList",Toast.LENGTH_SHORT).show()
+                }
+
+        }
     }
 }
